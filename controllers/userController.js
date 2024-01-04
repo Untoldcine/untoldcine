4590b822eac749ff2bab4addcd3ea7eb291ac052
Michael Deng <michaeldeng55@gmail.com>
2024-01-04 17:00:39 -0500

    port forwarding to access DB, installed dotenv

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,24 @@
+const mysql = require('mysql')
+require('dotenv').config();
 
 exports.getAllUsers = async(req, res) => {
-    res.json({"message": "we in here!"})
+
+    const connection = mysql.createConnection({
+        host: process.env.DB_HOST,
+        database: process.env.DB_NAME,
+        port: process.env.DB_PORT,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+    })
+    connection.connect()
+    connection.query('SELECT * FROM users', (queryError, results) => {
+        if (queryError){
+            console.error('Error ' + queryError);   
+        }
+        res.send(results)
+        connection.end()
+    })
+    console.log('complete');
+
 }
 
