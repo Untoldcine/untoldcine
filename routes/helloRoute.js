d6f1fb55ff0286a66d1a2b887c8c741415650fc7
Michael Deng <michaeldeng55@gmail.com>
2024-01-17 20:55:50 -0500

    cleanup prior to merge to main

--- routes/helloRoute.js
+++
@@ -1,5 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const controllers = require('../controllers/helloController.js');
-router.get('/', controllers.getHello)
-module.exports = router;
--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 const cors = require('cors');
 
-const helloRoute = require('./routes/helloRoute.js');
 const userRoute = require('./routes/userRoute.js')
 const seriesRoute = require('./routes/seriesRoute.js')
 const commentsRoute = require('./routes/commentsRoute.js')
@@ -15,8 +14,6 @@ const watchlistRoute = require('./routes/watchlistRoute.js')
 app.use(cors({"origin": 'http://localhost:3000'})); //temporary until we go live
 app.use(express.json())
 
-
-app.use('/api/hello', helloRoute);
 app.use('/api/user', userRoute);
 app.use('/api/series', seriesRoute);
 app.use('/api/comments', commentsRoute);
