4d73554da24190f965e319132fae18ce096fe93d
Michael Deng <michaeldeng55@gmail.com>
2024-02-01 13:42:27 -0500

    connecting login/signup, bcrypt and jwt auth

--- frontend/src/components/TextField/TextField.js
+++ frontend/src/components/TextField/TextField.js
@@ -9,7 +9,13 @@ export const TextField = ({ text, value, update, icon, field }) => {
     <div className="box">
       <i className={icon}></i>
       <input
-        type={field === "password" ? passVisible ? "text" : "password": 'text'}
+        type={
+          field === "password" || field === "checkPass"
+            ? passVisible
+              ? "text"
+              : "password"
+            : "text"
+        }
         placeholder={text}
         className="input"
         value={value}
@@ -17,26 +23,26 @@ export const TextField = ({ text, value, update, icon, field }) => {
           update((prev) => ({ ...prev, [field]: e.target.value }))
         }
       />
-      {field === "password" ? (
+      {/* Conditional Rendering of Show/Hide Password */}
+      {field === "password" || field === "checkPass" ? (
         passVisible ? (
           <i
             className="fa fa-regular fa-eye"
             id="pw-hidden"
             style={{ color: "white" }}
-            onClick = {() => setPassVisible(!passVisible)}
+            onClick={() => setPassVisible(!passVisible)}
           ></i>
         ) : (
           <i
             className="fa fa-regular fa-eye-slash"
             id="pw-hidden"
             style={{ color: "white" }}
-            onClick = {() => setPassVisible(!passVisible)}
+            onClick={() => setPassVisible(!passVisible)}
           ></i>
         )
-      ) : <div id = 'spacer'>
-        </div>}
+      ) : (
+        <div id="spacer"></div>
+      )}
     </div>
   );
 };
-
-// <FontAwesomeIcon icon="fa-regular fa-eye-slash" />
