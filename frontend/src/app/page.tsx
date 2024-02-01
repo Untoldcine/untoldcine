0881917da6106f175da5179165b6300231c85bcb
Michael Deng <michaeldeng55@gmail.com>
2024-02-01 11:40:32 -0500

    some changes to sign in for input fields

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,19 +1,22 @@
-'use client';
-import Image from "next/image";
+'use client'
+import {useState} from "react"
 import styles from "./page.module.css";
 import Link from "next/link";
 import { TextField } from "@/components/TextField/TextField.js";
 import { PrimaryButton } from "@/components/PrimaryButton/PrimaryButton.js";
 
 export default function Home() {
+
+  const [inputFields, setInputFields] = useState({username: '', email: '', password: ''})
+
   return (
     <main className={styles.main}>
       <div className={styles.popup}>
         <h4 className={styles.title}>Sign up</h4>
-        <TextField text="User Name" icon="fa fa-user icon" />
-        <TextField text="Email" icon="fa fa-envelope icon" />
-        <TextField text="Password" icon="fa fa-lock icon" />
-        <TextField text="Confirm Password" icon="fa fa-lock icon" />
+        <TextField text="User Name" icon="fa fa-user icon" value = {inputFields.username} update = {setInputFields} field = 'username'/>
+        <TextField text="Email" icon="fa fa-envelope icon" value = {inputFields.email} update = {setInputFields} field = 'email'/>
+        <TextField text="Password" icon="fa fa-lock icon" value = {inputFields.password} update = {setInputFields} field = 'password'/>
+        {/* <TextField text="Confirm Password" icon="fa fa-lock icon" /> */}
         <div>
           <div className={styles.container}>
             <span className={styles.checkmark}></span>
--- frontend/src/app/sign-in/page.tsx
+++ frontend/src/app/sign-in/page.tsx
@@ -1,15 +1,22 @@
+'use client'
+import {useState} from "react"
 import styles from "./page.module.css";
 import Link from "next/link";
 import { TextField } from "@/components/TextField/TextField.js";
 import { PrimaryButton } from "@/components/PrimaryButton/PrimaryButton.js";
 
 export default function Home() {
+
+  const [inputFields, setInputFields] = useState({email: '', password: ''})
+
+
+
   return (
     <main className={styles.main}>
       <div className={styles.popup}>
         <h4 className={styles.title}>Sign In</h4>
-        <TextField text="User Name" icon="fa fa-user icon" />
-        <TextField text="Password" icon="fa fa-lock icon" />
+        <TextField text="Email" value = {inputFields.email} update = {setInputFields} icon="fa fa-user icon" field = 'email'/>
+        <TextField text="Password" value = {inputFields.password} update = {setInputFields} icon="fa fa-lock icon" field = 'password'/>
         <div>
           <div className={styles.container}>
             <span className={styles.checkmark}></span>
